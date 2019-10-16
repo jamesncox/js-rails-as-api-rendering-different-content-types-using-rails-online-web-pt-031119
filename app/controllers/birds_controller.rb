419436cc582b47854a2fd88eea1ec6b3b22260d0
James Cox <jamesncox@yahoo.com>
2019-10-16 15:13:43 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,8 +1,8 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
-    # render plain: "Hello #{@birds[3].name}"
+    # render 'birds/index.html.erb'
+    render plain: "Hello #{@birds[3].name}"
 
   end
 end
